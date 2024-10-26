624c2f9d5bddeeb420d8d092e15c7aef22268213
Akshat Panchal <akshatp272004@gmail.com>
2024-10-26 10:07:52 +0530

    Removed dark/light mode feature

--- webapp.py
+++ webapp.py
@@ -4,54 +4,6 @@ from Data.data_of_weather import fetch_weather_data  # Import your function to f
 # Set up your Streamlit app
 st.set_page_config(page_title="Weather Data Visualization", layout="wide")
 
-# Initialize session state for dark mode if it doesn't exist
-if 'dark_mode' not in st.session_state:
-    st.session_state.dark_mode = False
-
-# Function to toggle dark mode
-def toggle_dark_mode():
-    st.session_state.dark_mode = not st.session_state.dark_mode
-
-# Add toggle icon in the top right corner
-icon = "🌙" if st.session_state.dark_mode else "🌞"
-toggle_button = st.button(icon, key="toggle_mode", on_click=toggle_dark_mode, help="Toggle Light/Dark Mode")
-
-# Apply CSS for dark mode if enabled
-if st.session_state.dark_mode:
-    st.markdown("""
-        <style>
-            body {
-                background-color: black;
-                color: white;
-            }
-            .stButton > button {
-                background-color: #444;
-                color: white;
-            }
-            .stTextInput input {
-                background-color: #444;
-                color: white;
-            }
-        </style>
-    """, unsafe_allow_html=True)
-else:
-    st.markdown("""
-        <style>
-            body {
-                background-color: white;
-                color: black;
-            }
-            .stButton > button {
-                background-color: #f0f0f0;
-                color: black;
-            }
-            .stTextInput input {
-                background-color: white;
-                color: black;
-            }
-        </style>
-    """, unsafe_allow_html=True)
-
 # Title and description
 st.title("Weather Data Visualization")
 st.markdown("Enter a city name to get the current weather details!")
